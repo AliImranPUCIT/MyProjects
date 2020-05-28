710cd7e2bcdbb642f2fc402ce6fb6712d2ae3c41
AliImranPUCIT <66085862+AliImranPUCIT@users.noreply.github.com>
2020-05-28 23:52:24 +0500

    Add files via upload
    This repository project contains EAD final project of Class SE batch 2017 with proper model view controller touch in C#

---
+++ HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment_3.Models;
+
+namespace Assignment3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        DataClasses_1DataContext dc = new DataClasses_1DataContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SelectHouse()
+        {
+            return View(dc.Houses.ToList());
+        }
+
+        public ActionResult ViewHouse()
+        {
+            string Area = Request["Area"];
+            var query = dc.Houses.Where(h => h.Area == Area);
+            return View(query);
+        }
+
+        public ActionResult AddHouse()
+        {
+            return View();
+        }
+
+        public ActionResult AddHouse_2()
+        {
+            Houses h = new Houses();
+            h.Area = Request["Area"];
+            h.Address = Request["Address"];
+            h.Marlas = Request["Marlas"];
+            h.Price = Request["Price"];
+
+            dc.Houses.InsertOnSubmit(h);
+            dc.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult EditHouse(int Id)
+        {
+            return View(dc.Houses.First(h => h.Id == Id));
+        }
+
+        public ActionResult EditHouse_2(int Id)
+        {
+            var q = dc.Houses.First(h => h.Id == Id);
+            q.Area = Request["Area"];
+            q.Address = Request["Address"];
+            q.Marlas = Request["Marlas"];
+            q.Price = Request["Price"];
+
+            dc.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
